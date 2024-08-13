9a094f07a3300463196c82ff5d568ee6388b7e8a
rosenclever <rosenclever@gmail.com>
2024-08-13 20:52:48 -0300

    metodos soma e multiplicacao

---
+++ AulasPOO/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AulasPOO.uteis;
+int opcao = 0, num1, num2;
+do
+{
+    Console.WriteLine("Escolha uma opção:");
+    Console.WriteLine("1) Somar");
+    Console.WriteLine("2) Multiplicar");
+    Console.WriteLine("3) Fatorial");
+    Console.WriteLine("0) Sair");
+    opcao = Convert.ToInt32(Console.ReadLine());
+    switch (opcao)
+    {
+        case 0:
+            Console.WriteLine("Fim do Programa");
+            break;
+        case 1:
+            LerDoisNumerosInteiros();
+            var calc = new Calculadora();
+            int resultado = calc.Soma(num1, num2);
+            Console.WriteLine($"O resultado da soma de {num1} com {num2} é {resultado}");
+            break;
+        case 2:
+            LerDoisNumerosInteiros();
+            Console.WriteLine($"O resultado da multiplicação de {num1} por {num2} é {Calculadora.Multiplica(num1, num2)}");
+            break;
+        default:
+            Console.WriteLine("Opcao Inválida");
+            break;
+    }
+} while (opcao != 0);
+
+void LerDoisNumerosInteiros()
+{
+    Console.WriteLine("Informe o primeiro número:");
+    num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe o segundo número:");
+    num2 = int.Parse(Console.ReadLine());
+}
---
+++ AulasPOO/uteis/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulasPOO.uteis
+{
+    public class Calculadora
+    {
+        public int Soma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Multiplica(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
