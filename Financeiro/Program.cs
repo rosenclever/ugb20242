8d7cec752d451f79073423c6374562965961b975
rosenclever <rosenclever@gmail.com>
2024-08-27 21:04:08 -0300

    Projeto financeiro com conta

---
+++ Financeiro/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Financeiro;
+
+Conta conta1 = new Conta(1);
+Conta conta2 = new Conta();
+//conta1.Numero = 1;
+Console.WriteLine($"O número da conta é {conta1.Numero}");
